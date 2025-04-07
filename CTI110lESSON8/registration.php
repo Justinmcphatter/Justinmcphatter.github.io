7ef72a2ac12a37d9b19293cbcdd688d964efc07c
Justinmcphatter <capitalj98@yahoo.com>
2025-04-07 12:17:37 -0400

    Gradebook file uploaded

---
+++ CTI110lESSON8/registration.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Tuition Receipt</title>
+        <link rel="stylesheet" type="text/css" href="sample.css">
+    </head>
+    <body>
+        <h1>Tuition Receipt</h1>
+        <?php
+            $fname = $_POST['fname'];
+            $lname = $_POST['lname'];
+            $numCourse = $_POST['numCourse'];
+            $tuition = $numCourse * 200.00;
+
+            print("<p>$fname $lname</p>");
+            print("<p>You have registered $numCourse courses.</p>");
+            print("<p>Your tuition is ".number_format($tuition, 2)."</p>");
+        ?>
+        <br><br>
+        <a href="course.html">Back to Courses</a>
+    </body>
+</html>
