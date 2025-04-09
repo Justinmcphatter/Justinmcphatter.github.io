6c1d76fda58e7fb941f08c9345cdd125dc4725b2
Justinmcphatter <capitalj98@yahoo.com>
2025-04-09 11:02:43 -0400

    update

--- Gradebook/gradebook.js
+++ Gradebook/gradebook.js
@@ -1,9 +1,16 @@
-<?xml version="1.0" encoding="UTF-8" ?>
-<NotepadPlus>
-    <Session activeView="0">
-        <mainView activeIndex="0">
-            <File firstVisibleLine="0" xOffset="0" scrollWidth="720" startPos="329" endPos="329" selMode="0" offset="0" wrapCount="1" lang="JavaScript" encoding="-1" userReadOnly="no" filename="C:\Users\Capit\OneDrive\Desktop\Justinmcphatter.github.io\Gradebook\gradebook.js" backupFilePath="" originalFileLastModifTimestamp="-1106293221" originalFileLastModifTimestampHigh="31172576" tabColourId="-1" RTL="no" tabPinned="no" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="512" mapWrapIndentMode="-1" mapIsWrap="no" />
-        </mainView>
-        <subView activeIndex="0" />
-    </Session>
-</NotepadPlus>
+// Function to fetch grade data from the database
+function fetchGradeData() {
+    console.log("Fetching grade data...");
+    // This stub will be implemented later to get data from PostgreSQL
+    return [];
+}
+
+// Function to populate the gradebook table with data
+function populateGradebook() {
+    console.log("Populating gradebook...");
+    const data = fetchGradeData();
+    // This stub will be implemented later to populate the table
+}
+
+// Call populateGradebook when the page loads
+window.onload = populateGradebook;
