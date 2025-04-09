f3e27be5c1e5cb2add24e63e30042c19130daf02
Justinmcphatter <capitalj98@yahoo.com>
2025-04-09 10:54:45 -0400

    update

--- Gradebook/gradebook.js
+++ Gradebook/gradebook.js
@@ -1,9 +1,9 @@
-function fetchGradeData() {
-    console.log("fetchGradeData function called");
-    // This function will later fetch data from PostgreSQL
-}
-
-function populateGradebook(data) {
-    console.log("populateGradebook function called with data:", data);
-    // This function will later populate the table with the fetched data
-}
+<?xml version="1.0" encoding="UTF-8" ?>
+<NotepadPlus>
+    <Session activeView="0">
+        <mainView activeIndex="0">
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="720" startPos="329" endPos="329" selMode="0" offset="0" wrapCount="1" lang="JavaScript" encoding="-1" userReadOnly="no" filename="C:\Users\Capit\OneDrive\Desktop\Justinmcphatter.github.io\Gradebook\gradebook.js" backupFilePath="" originalFileLastModifTimestamp="-1106293221" originalFileLastModifTimestampHigh="31172576" tabColourId="-1" RTL="no" tabPinned="no" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="512" mapWrapIndentMode="-1" mapIsWrap="no" />
+        </mainView>
+        <subView activeIndex="0" />
+    </Session>
+</NotepadPlus>
---
+++ Gradebook/gradebook.js.inCaseOfCorruption.bak
@@ -0,0 +1,9 @@
+function fetchGradeData() {
+    console.log("fetchGradeData function called");
+    // This function will later fetch data from PostgreSQL
+}
+
+function populateGradebook(data) {
+    console.log("populateGradebook function called with data:", data);
+    // This function will later populate the table with the fetched data
+}
